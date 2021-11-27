408b686c6c7b4c3ffac5ea141c5cbd6f7a4617c5
ponktacoloy <ponktacology@mailfence.com>
2021-11-27 13:24:55 +0100

    kit editor menu refactoring

--- src/main/java/country/pvp/practice/player/PlayerService.java
+++ src/main/java/country/pvp/practice/player/PlayerService.java
@@ -2,6 +2,7 @@ package country.pvp.practice.player;
 
 import com.google.inject.Inject;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import country.pvp.practice.data.mongo.MongoRepository;
 
 public class PlayerService extends MongoRepository<PracticePlayer> {
@@ -9,5 +10,15 @@ public class PlayerService extends MongoRepository<PracticePlayer> {
     @Inject
     public PlayerService(MongoDatabase database) {
         super(database);
+    }
+
+    @Override
+    public void load(PracticePlayer entity) {
+        org.bson.Document document = database.getCollection(entity.getCollection()).find(Filters.eq("_id", entity.getId())).first();
+        if (document == null) {
+            save(entity);
+            return;
+        }
+        entity.applyDocument(document);
     }
 }
